dc73846dd8850cdf18f59478eac0b14785fa5c01
Gustavo Muller <contatogustavomuller@gmail.com>
2017-06-20 16:16:50 -0300

    Adicionando comentários

--- ARDUINO/arduinoNano/arduinoNano.ino
+++ ARDUINO/arduinoNano/arduinoNano.ino
@@ -2,6 +2,7 @@
 
 String data;
 
+//Definição dos pinos do sensor RFID
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
@@ -20,18 +21,20 @@ void loop()
   if( ! mfrc522.PICC_IsNewCardPresent()){
     return;
   }
-  // Select one of the cards
+  // Seleciona um dos cartões
   if( ! mfrc522.PICC_ReadCardSerial()){
     return;
   }
   String conteudo= "";
   byte letra;
+  //Concatenação dos dados lidos dos cartões em Hexadecimal
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   conteudo.toUpperCase();
+  //Verifica se os dados lidos são válidos, se sim os manda para o Arduino Uno
   if(conteudo.substring(1)!=""){
     Serial.print(conteudo.substring(1));
     delay(1000);
--- ARDUINO/arduinoUno/arduinoUno.ino
+++ ARDUINO/arduinoUno/arduinoUno.ino
@@ -1,27 +1,24 @@
 #include <Ethernet.h>
 
+//Configurações da rede local e estabelecimento do ip destino
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] = { ***, ***, ***, *** };
 byte es[] = { 8, 8, 8, 8 };
 byte gateway[] = { ***, ***, ***, *** };
 byte subnet[] = { 255, 255, 252, 0 };
-byte server[] = { ***, ***, ***, *** }; // Botanicapp
+byte server[] = { ***, ***, ***, *** };
 
 EthernetClient client;
 
 void setup()
 {
   Ethernet.begin(mac, ip, es, gateway, subnet); 
-  //Ethernet.begin(mac, ip);
   Serial.begin(9600);
-  
-  //delay(1000);
 
-  //Serial.println("connecting...");
-  
+  //Verifica se a conexão com o server é possível
   if (client.connect(server, 80)) {
     Serial.println("connected");
-    client.println("GET /cleyton/estagiarios/acesso.php?id=****&cod=**** HTTP/1.0"); //perceptron.php?id=*****
+    client.println("GET /cleyton/estagiarios/acesso.php?id=****&cod=**** HTTP/1.0");
     client.println();
     delay(3000);
   } else {
@@ -33,7 +30,7 @@ void setup()
 
 void loop()
 {
-  
+  //Printando informação recebida do server
   if (client.available()) {
     char c = client.read();
     Serial.print(c);
@@ -47,6 +44,7 @@ void loop()
     //  ;
   }
 
+  //Verifica se está recebendo alguma coisa do Arduino Nano e guarda as informações
   if(Serial.available()>0){
     String a = Serial.readString(); 
     String b = a.substring(0,2);
@@ -59,6 +57,7 @@ void loop()
   
 }
 
+//Envia os dados coletados para o server
 int enviaDado(String id){
   Serial.println("connecting...");
   if (client.connect(server, 80)) {
